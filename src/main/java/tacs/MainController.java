6b1d25ef02112f9b0c0fe49046c3fe8714eeef58
Gaston Contreras <gastongastongg98@gmail.com>
2019-12-29 14:40:34 -0300

    Add cookie management

--- src/main/java/tacs/MainController.java
+++ src/main/java/tacs/MainController.java
@@ -1,19 +1,29 @@
 package tacs;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class MainController {
 
-    private List<Account> accounts;
+    private String TOKEN_SUFFIX = "tokensecreto";
+    private List<Account> accounts = new LinkedList<>();
 
     @GetMapping("/")
-    public String home() {
+    public String home(@CookieValue(value = "token", defaultValue = "") String token) {
+        String username = usernameFromToken(token);
+        if (accounts.stream().noneMatch(account -> account.getUsername().equals(username))) {
+            return "redirect:/login";
+        }
         return "home";
     }
 
@@ -23,12 +33,16 @@ public class MainController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
-        if (username.equals("admin") && password.equals("admin")) {
-            return "redirect:/";
-        } else {
-            return "redirect:/error";
+    public String login(HttpServletResponse response, @RequestParam("username") String username, @RequestParam("password") String password) {
+        Optional<Account> maybeAccount = accounts.stream().filter(account -> account.getUsername().equals(username)).findFirst();
+        if (maybeAccount.isPresent()) {
+            Account account = maybeAccount.get();
+            if (password.equals(account.getPassword())) {
+                response.addCookie(new Cookie("token", username + TOKEN_SUFFIX));
+                return "redirect:/";
+            }
         }
+        return "redirect:/error";
     }
 
     @GetMapping("/register")
@@ -39,7 +53,14 @@ public class MainController {
     @PostMapping("/register")
     public String register(@RequestParam("username") String username, @RequestParam("password") String password) {
         Account newAccount = new Account(username, password);
+        if (accounts.stream().anyMatch(account -> account.getUsername().equals(username))) {
+            return "redirect:/error"; // TODO: Add better error message
+        }
         accounts.add(newAccount);
         return "redirect:/login";
+    }
+
+    private String usernameFromToken(@CookieValue(value = "token", defaultValue = "") String token) {
+        return token.replaceAll(TOKEN_SUFFIX, "");
     }
 }
