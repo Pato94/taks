ea9795d0fff7b508ca2efb11f45bd9e69e807845
Gaston Contreras <gastongastongg98@gmail.com>
2019-12-29 13:27:00 -0300

    Add basic register screen with own css

--- src/main/java/tacs/MainController.java
+++ src/main/java/tacs/MainController.java
@@ -5,8 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class MainController {
+
+    private List<Account> accounts;
 
     @GetMapping("/")
     public String home() {
@@ -25,5 +29,17 @@ public class MainController {
         } else {
             return "redirect:/error";
         }
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestParam("username") String username, @RequestParam("password") String password) {
+        Account newAccount = new Account(username, password);
+        accounts.add(newAccount);
+        return "redirect:/login";
     }
 }
